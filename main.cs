ffaf17cb4c7901c98975e6ceb09a2b8f7ca6b875
Juan Sebastian <>
2023-02-09 00:03:57 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+  numerosPrimos(1 , 100);
+
+  static void numerosPrimos (int numInit,int numFinish){
+    while (numInit <= numFinish){
+       int contador = 1;
+       int  numDivExactas = 0;
+       while (contador <= numInit){
+         if (numInit % contador == 0){
+           numDivExactas = numDivExactas +1;
+           
+         }
+         contador = contador + 1;
+       }
+      if ( numDivExactas == 2){
+        Console.WriteLine(numInit);
+      }
+      numInit = numInit + 1;
+      }
+  }
+    }
+}
